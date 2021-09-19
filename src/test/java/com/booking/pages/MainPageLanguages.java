5320eaa07c422d12ab82debd73d04ccbcce06ba1
maksim <maximum38@ya.ru>
2021-09-20 01:45:06 +0300

    languages switch

---
+++ src/test/java/com/booking/pages/MainPageLanguages.java
@@ -0,0 +1,58 @@
+package com.booking.pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class MainPageLanguages {
+
+    //кнопка выбора языка
+    private final SelenideElement LANGUAGE_SELECTION = $x("//button[@data-modal-id='language-selection']");
+
+    //кнопки языков
+    private final SelenideElement EN_GB = $x("//div[@lang='en-gb']");
+    private final SelenideElement EN_US = $x("//div[@lang='en-us']");
+    private final SelenideElement DE = $x("//div[@lang='de']");
+    private final SelenideElement IT = $x("//div[@lang='it']");
+    private final SelenideElement PL = $x("//div[@lang='pl']");
+
+    //подтверждающие ссылки
+    private final SelenideElement EN_GB_CHECK = $x("//link[@href='https://www.booking.com/index.en-gb.html'][@rel='canonical']");
+    private final SelenideElement EN_US_CHECK = $x("//span[contains(text(),'Your current language is English (US)')]");
+    private final SelenideElement DE_CHECK = $x("//span[contains(text(),'Ihre aktuelle Sprache ist Deutsch')]");
+    private final SelenideElement IT_CHECK = $x("//span[contains(text(),'Lingua attuale: Italiano')]");
+    private final SelenideElement PL_CHECK = $x("//span[contains(text(),'Wybrany język to Polski')]");
+
+    public boolean switchToGBCheck(){
+        LANGUAGE_SELECTION.click();
+        EN_GB.click();
+        return EN_GB_CHECK.is(Condition.exist);
+    }
+
+    public boolean switchToUSCheck(){
+        LANGUAGE_SELECTION.click();
+        EN_US.click();
+        return EN_US_CHECK.is(Condition.exist);
+    }
+
+    public boolean switchToDECheck(){
+        LANGUAGE_SELECTION.click();
+        DE.click();
+        return DE_CHECK.is(Condition.exist);
+    }
+
+    public boolean switchToITCheck(){
+        LANGUAGE_SELECTION.click();
+        IT.click();
+        return IT_CHECK.is(Condition.exist);
+    }
+
+    public boolean switchToPLCheck(){
+        LANGUAGE_SELECTION.click();
+        PL.click();
+        return PL_CHECK.is(Condition.exist);
+    }
+
+
+}
---
+++ src/test/java/com/booking/ui/TestLanguageChange.java
@@ -0,0 +1,64 @@
+package com.booking.ui;
+
+import com.booking.pages.MainPageLanguages;
+import com.booking.pages.RegistrationForm;
+import com.codeborne.selenide.commands.As;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import ui.core.Driver;
+
+public class TestLanguageChange {
+
+    static MainPageLanguages mainPageLanguages;
+
+    @BeforeAll
+    public static void getDriver(){
+        Driver.getDriver();
+        mainPageLanguages = new MainPageLanguages();
+        //logManager = LogManager.getLogger(TestTopMenuLinksSelenide.class);
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().savePageSource(false).screenshots(true));
+    }
+
+    @AfterAll
+    public static void closeDriver(){
+        Driver.closeDriver();
+    }
+
+    @Test
+    @Step("Switch to EN_GB")
+    @Order(1)
+    public void testChangeEngGB(){
+        Assertions.assertTrue(mainPageLanguages.switchToGBCheck(), "Неправильно поменяли язык на EN_GB");
+    }
+
+    @Test
+    @Step("Switch to EN_US")
+    @Order(2)
+    public void testChangeEngUS(){
+        Assertions.assertTrue(mainPageLanguages.switchToUSCheck(),"Неправильно поменяли язык на EN_US");
+    }
+
+    @Test
+    @Step("Switch to DE")
+    @Order(3)
+    public void testChangeDE(){
+        Assertions.assertTrue(mainPageLanguages.switchToDECheck(),"Неправильно поменяли язык на DE");
+    }
+
+    @Test
+    @Step("Switch to IT")
+    @Order(4)
+    public void testChangeIT(){
+        Assertions.assertTrue(mainPageLanguages.switchToITCheck(),"Неправильно поменяли язык на IT");
+    }
+
+    @Test
+    @Step("Switch to PL")
+    @Order(5)
+    public void testChangePL(){
+        Assertions.assertTrue(mainPageLanguages.switchToPLCheck(),"Неправильно поменяли язык на PL");
+    }
+
+}
